0fcccf6d40f013af565fceebac67bff4c353075d
GCaringi <giannicaringi94@gmail.com>
2022-05-24 16:51:00 +0200

    Finish layout

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const app = new Vue({
     el: "#app",
     data: {
+        showNotify: true,
         contacts: [
     {
         name: 'Michele',
@@ -146,23 +147,29 @@ const app = new Vue({
         avatar: '_8',
         visible: true,
         messages: [
-            {
-                date: '10/01/2020 15:30:55',
-                message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                status: 'received'
-            },
-            {
-                date: '10/01/2020 15:50:00',
-                message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                status: 'sent'
-            },
-            {
-                date: '10/01/2020 15:51:00',
-                message: 'OK!!',
-                status: 'received'
-            }
-        ],
-    }
-]
-    }
+                {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                    status: 'received'
+                 },
+                {
+                    date: '10/01/2020 15:50:00',
+                    message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:51:00',
+                    message: 'OK!!',
+                    status: 'received'
+                }
+            ],
+        }
+    ]
+    },
+    methods: {
+        showNotifys(){
+            console.log("ENTRO")
+            this.showNotify = false;
+        }
+    },
 })
